921661c48cf9f3fbff08093a5132bd924d9013c2
BHARATH <bharath122020@gmail.com>
2025-05-10 12:31:34 +0530

    commit code as niraml said

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\91637\Desktop\bharath\v4\venv
+set VIRTUAL_ENV=C:\Users\91637\Desktop\bharath\portfolio\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
